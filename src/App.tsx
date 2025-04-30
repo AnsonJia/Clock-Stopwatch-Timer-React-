4ab988812396d5fc206455886ebfe31f3250b7ca
Anson Jia <ansonjia123@gmail.com>
2025-04-30 19:30:56 -0400

    updated app component with stopwatch and timer paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Clock from "./DigitalClock/Clock"
+import Stopwatch from './Stopwatch/Stopwatch';
+import Sidebar from "./Sidebar/Sidebar"
+import Timer from './Timer/Timer';
 
 function App() {
 
   return (
-    <>
-     <Clock/>
-    </>
+    <Router>
+      <div>
+        <Sidebar />
+        <div>
+          <Routes>
+            
+            <Route path="/clock" element={<Clock />} />
+            <Route path="/stopwatch" element={<Stopwatch />} />
+            <Route path="/timer" element={<Timer />} />
+            
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
