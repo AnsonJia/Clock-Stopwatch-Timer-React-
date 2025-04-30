a21ea0947087f0a054b84f25ccabcd047e2d9acd
Anson Jia <ansonjia123@gmail.com>
2025-04-30 19:29:09 -0400

    updated sidebar component

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -5,8 +5,8 @@ function Sidebar() {
   return (
     <div className={styles.sidebar}>
         <Link to="/clock" className={styles.item}><img src='./src/assets/time-outline.svg'/>Time</Link>
-        <Link to="/stopwatch" className={styles.item}>Stopwatch</Link>
-        <Link to="/timer" className={styles.item}>Timer</Link>
+        <Link to="/stopwatch" className={styles.item}><img src='./src/assets/stopwatch-svgrepo-com.svg'/>Stopwatch</Link>
+        <Link to="/timer" className={styles.item}><img src='./src/assets/timer-outline.svg'/>Timer</Link>
         
     </div>
   );
