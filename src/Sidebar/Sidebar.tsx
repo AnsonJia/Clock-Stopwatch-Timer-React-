effa6a93859a2fb0c04036df892bfa8c68d44cea
Anson Jia <ansonjia123@gmail.com>
2025-05-02 12:38:19 -0400

    added functionality to open and close sidebar

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,14 +1,26 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import styles from './Sidebar.module.css';
 
 function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.sidebar}>
+    <>
+      <div className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
         <Link to="/clock" className={styles.item}><img src='./src/assets/time-outline.svg'/>Time</Link>
         <Link to="/stopwatch" className={styles.item}><img src='./src/assets/stopwatch-svgrepo-com.svg'/>Stopwatch</Link>
         <Link to="/timer" className={styles.item}><img src='./src/assets/timer-outline.svg'/>Timer</Link>
-        
-    </div>
+      </div>
+    
+    
+    
+    <button className={styles.openbtn} onClick={()=>setIsOpen(prev => !prev)}>&#9776;</button>
+    
+    
+   
+    
+    </>
   );
 }
 
