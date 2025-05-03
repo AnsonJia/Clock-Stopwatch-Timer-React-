0f0afe8a9770658c2e8723fdc25ccac70d07ea62
Anson Jia <ansonjia123@gmail.com>
2025-05-03 09:24:55 -0400

    fixed sidebar hover causing border to disapear

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -8,9 +8,24 @@ function Sidebar() {
   return (
     <>
       <div className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
-        <Link to="/clock" className={styles.item}><img src='./src/assets/time-outline.svg'/>Time</Link>
-        <Link to="/stopwatch" className={styles.item}><img src='./src/assets/stopwatch-svgrepo-com.svg'/>Stopwatch</Link>
-        <Link to="/timer" className={styles.item}><img src='./src/assets/timer-outline.svg'/>Timer</Link>
+        <Link to="/clock" className={styles.item}>
+        <div className={styles.content}>
+        <img src='./src/assets/time-outline.svg'/>Time
+        </div>
+        </Link>
+        
+        <Link to="/stopwatch" className={styles.item}>
+        <div className={styles.content}>
+        <img src='./src/assets/stopwatch-svgrepo-com.svg'/>Stopwatch
+        </div>
+        </Link>
+        
+        <Link to="/timer" className={styles.item}>
+        <div className={styles.content}>
+        <img src='./src/assets/timer-outline.svg'/>Timer
+        </div>
+        </Link>
+
       </div>
     
     
