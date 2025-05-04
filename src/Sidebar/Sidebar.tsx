6bdc51a9186f1fe27023af891e7d961515542f20
Anson Jia <ansonjia123@gmail.com>
2025-05-04 09:37:31 -0400

    updated sidebar to display active page

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,26 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import styles from './Sidebar.module.css';
 
 function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
+  const page = useLocation();
 
   return (
     <>
       <div className={`${styles.sidebar} ${isOpen ? styles.open : styles.closed}`}>
-        <Link to="/clock" className={styles.item}>
+        <Link to="/clock" className={`${styles.item} ${page.pathname === '/clock' ? styles.active : ''}`}>
           <div className={styles.content}>
             <img src='./src/assets/time-outline.svg'/>Time
           </div>
         </Link>
         
-        <Link to="/stopwatch" className={styles.item}>
+        <Link to="/stopwatch" className={`${styles.item} ${page.pathname === '/stopwatch' ? styles.active : ''}`}>
           <div className={styles.content}>
             <img src='./src/assets/stopwatch-svgrepo-com.svg'/>Stopwatch
           </div>
         </Link>
         
-        <Link to="/timer" className={styles.item}>
+        <Link to="/timer" className={`${styles.item} ${page.pathname === '/timer' ? styles.active : ''}`}>
           <div className={styles.content}>
             <img src='./src/assets/timer-outline.svg'/>Timer
           </div>
